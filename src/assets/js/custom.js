0fe0b2ffb8c6f4aeb9689fdaef8496ad1a46830c
imashaaaa <imashasithmini2@gmail.com>
2022-03-10 10:20:39 +0530

    header footer disabled

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -8,6 +8,7 @@ function saveDocument() {
 function loadDocument() {
     TXTextControl.loadDocument(TXTextControl.StreamType.HTMLFormat,
         btoa('<strong>Test</strong>'));
+        TXTextControl.tables.add(3, 3)
 }
 
 let checked = true; 
@@ -26,16 +27,37 @@ function toggleVerticalRuler() {
 function textEditor() {
 
     console.log('asfdf')
-    //TXTextControl.HeaderFooter.GetItem( TXTextControl.HeaderFooterType.Header); 
-    //console.log(TXTextControl.HeaderFooter.GetItem( TXTextControl.HeaderFooterType.Header))
-    //TXTextControl.HeaderFooterCollection.add(Headers Header)
-    //TXTextControl.HeaderFooterCollection.add(new TXTextControl )
-    //TXTextControl.textEditor.
-    //TX.backColor = '#000000'
-    //Text.
-    //TXTextControl.HeaderFooterType.Header('asfdf')
-    //console.log(TXTextControl.HeaderFooterType.Header('asfdf'))
-    //console.log(TXTextControl.HeaderFooterCollection.add(TXTextControl.HeaderFooterType.Header))
-    //TXTextControl.HeaderFooterCollection.add(<string> headerFooterType,)
+
+    //Add a header
+
+    TXTextControl.HeaderFooterCollection.add(TXTextControl.HeaderFooterType.Header);
+
+    TXTextControl.HeaderFooterCollection.Add(TXTextControl.HeaderFooterType.Header)
+    var header = TXTextControl.HeaderFooterCollection.getItem(TXTextControl.HeaderFooterType.Header)
+
+    console.log(header)
+
+    header.Selection.Text = 'hi'
+
+    TXTextControl.tables.add(3, 3)
+ 
+    
+  }
+
+  //Hide Header and footer
+  function toggleRibbonElement()
+  {
+    //Hide Header
+    document.getElementById("ribbonTabInsert_drpDnBtnInsertHeader" ).style.pointerEvents= 'none'
+
+    //Hide Footer
+    document.getElementById("ribbonTabInsert_drpDnBtnInsertFooter" ).style.pointerEvents= 'none'
   }
   
+
+
+ 
+  
+
+
+    
